5b8bb30ab6fe6efd05c2c049f9e1256c298b72b1
praveen875 <rececepiraveen@gmail.com>
2023-09-11 15:29:43 +0530

    test4

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
   tags = {
-    Name = "test2_instance"
+    Name = "test3_instance"
   }
 }
